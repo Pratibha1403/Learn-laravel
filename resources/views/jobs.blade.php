776daebec1da7cdfe9d52c447a339cbf7f5c3e81
Pratibha1403 <101884035+Pratibha1403@users.noreply.github.com>
2024-11-12 22:56:46 +0530

    changing lazy loading to eager loading (N+1 Problem)

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,14 +2,17 @@
     <x-slot:heading>
         Job Listing
     </x-slot:heading>
-    <ul>
+    <div class="space-y-4">
         @foreach ( $job as $jobs )
-            <li>
-                <a href="/jobs/{{$jobs['id']}}">
-                    <strong>{{$jobs['title']}} :</strong> Pay {{$jobs['salary']}} Per Year
-                </a>
-            </li>
+
+        <a href="/jobs/{{$jobs['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+            <div class="text-blue-500 font-bold">{{$jobs->employer->name}}</div>
+            <div>
+            <strong>{{$jobs['title']}} :</strong> Pay {{$jobs['salary']}} Per Year
+            </div>
+        </a>
+
         @endforeach
-    </ul>
+    </div>
 
 </x-layout>
